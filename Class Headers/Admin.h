e8d9af21547e492fe20d592411657915d7a14975
zerogravity2000 <32613635+zerogravity2000@users.noreply.github.com>
2017-11-22 23:47:09 +0200

    Create Admin.h

---
+++ Class Headers/Admin.h
@@ -0,0 +1,19 @@
+#ifndef ADMIN_H_INCLUDED
+#define ADMIN_H_INCLUDED
+
+#include "object.h"
+#include "Level.h"
+
+using namespace std;
+
+class Adminmenu : public Level
+{
+public:
+    Adminmenu();
+    ~Adminmenu();
+  void AddButton();
+  static int point;
+  void PrintItems();
+};
+
+#endif // ADMIN_H_INCLUDED
