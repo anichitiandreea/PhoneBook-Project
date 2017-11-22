2a2844fd72df2dde29af1bb6c846e6cc0e8e71a1
zerogravity2000 <32613635+zerogravity2000@users.noreply.github.com>
2017-11-22 23:34:45 +0200

    Add files via upload

---
+++ Interfaces/ILevel.h
@@ -0,0 +1,16 @@
+#ifndef ILEVEL_H_INCLUDED
+#define ILEVEL_H_INCLUDED
+
+#include <string>
+
+class ILevel
+{
+    public:
+    virtual void PrintItems()                 = 0;
+    virtual void RemoveItem()                 = 0;
+    virtual void AddItem(const  std::string&) = 0;
+    virtual void AddButton()                  = 0;
+};
+
+
+#endif // ILEVEL_H_INCLUDED
